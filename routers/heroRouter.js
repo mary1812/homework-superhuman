e1ee80126711246b38957eb25471c8271949f3a0
Mary <mariyapiligrim3@gmail.com>
2021-11-24 03:33:03 +0200

    Add router for get

--- routers/heroRouter.js
+++ routers/heroRouter.js
@@ -1,6 +1,8 @@
 const heroRouter = require('express').Router();
 const HeroController = require('../controllers/heroController');
 
+heroRouter.get('/', HeroController.getHero);
+
 heroRouter.post('/', HeroController.createHero);
 
 heroRouter.patch('/:id', HeroController.updateHero);
